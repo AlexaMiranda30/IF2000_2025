754336876a1817ccb035ca07b32c8815ac582b43
AlexaMiranda30 <milalex3003@gmail.com>
2025-08-14 11:20:36 -0600

    Mi Primer commit en Java

---
+++ IF2000_IntroduccionProgra/src/main/java/com/mycompany/if2000_introduccionprogra/IF2000_IntroduccionProgra.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.if2000_introduccionprogra;
+
+/**
+ *
+ * @author almi0
+ */
+public class IF2000_IntroduccionProgra {
+
+    public static void main(String[] args) {
+      
+        
+        
+        //Data Types
+        String name = "Lionel";
+        int age = 40;
+        
+          System.out.println("Hello World!");
+          System.out.println ("Name: " + name);
+          System.out.println ("Age: " + age);
+        
+        /* 
+           Tipos de datos numericos: 
+          int,long,short,float,double,numeric,money,decimal
+
+           Manejo de fechas:
+           Date
+           
+           Cadena de caracteres:
+            String
+
+           Objetos:
+           Object
+
+           Data Structures:
+           Arrays or Vectors: int[] x;
+                              int [] [] y;
+           True/False
+           boolean
+           */
+        
+    }
+}
